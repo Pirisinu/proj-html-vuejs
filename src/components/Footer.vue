b28064ffa122fccd5a7d77d4fea407b171d848b1
Pirisinu <palazzialessandro2@gmail.com>
2023-10-19 12:34:53 +0200

    [ADD]Scss set + [ADD]BASIC COMPONENTS

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+
+<template>
+  <h1>Footer</h1>
+</template>
+
+<style lang="scss" scoped>
+@use '../scss/main.scss' as *;
+h1{
+  color: $black;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+
+<template>
+  <h1>Header</h1>
+</template>
+
+<style lang="scss" scoped>
+@use '../scss/main.scss' as *;
+h1{
+  color: $black;
+}
+</style>
