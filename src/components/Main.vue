4598f4e536ba5bfefc817c10c7aae93ef6aaf66b
Pirisinu <palazzialessandro2@gmail.com>
2023-10-20 11:23:43 +0200

    [ADD]Menu.js + import card components in Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,53 +1,53 @@
 <script>
 import TxtCard from './TxtCard.vue';
+import mainMenu from '../data/mainMenu.js';
+import MenuCard from './MenuCard.vue';
+import CriticsCard from './CriticsCard.vue'; 
+
 export default {
   name: 'Main',
   components:{
-    TxtCard
+    TxtCard,
+    MenuCard,
+    CriticsCard
+  },
+  data(){
+    return{
+      mainMenu
+    }
+  },
+  mounted(){
+    console.log(mainMenu);
   }
 }
 </script>
 
 
 <template>
-<TxtCard
-  class=""
-  :title="'Ciao'"
-  :heading="'Ciao4'"
-  :description="'Custom Description '"
-  :buttonText="'Click Me'"
->
-  <template v-slot:left>
-    <span class="line"></span>
-  </template>
-</TxtCard>
-<TxtCard
-  class=""
-  :title="'Ciao'"
-  :heading="'Ciao4'"
-  :description="'Custom Description'"
-  :buttonText="'Click Me'"
->
-  <template v-slot:left>
-    <span class="line"></span>
-  </template>
-</TxtCard>
-<TxtCard
-  class=""
-  :title="'Ciao'"
-  :heading="'Ciao4'"
-  :description="'Custom Description'"
-  :buttonText="'Click Me'"
->
-  <template v-slot:left>
-    <span class="line"></span>
-  </template>
-</TxtCard>
-      
-    
+  <!-- <section class="container food-experience">
+    <div>
+      <div></div>
+      <div>
+        <img src="../assets/img/info22x.jpg" alt="">
+      </div>
+    </div>
+    <div></div>
+  </section> -->
+  <section class="">img+card</section>
+  <section>
+
+  </section>
+
 </template>
 
 <style lang="scss" scoped>
 @use '../scss/main.scss' as *;
+
+.food-experience{
+
+  img{
+
+  }
+}
 
 </style>
---
+++ src/data/mainMenu.js
@@ -0,0 +1,79 @@
+const menu = [
+  {
+    nameMenu: 'Hors d\'oeuvres',
+    plate: [
+      {
+        namePlate: 'ahi salmon nigiri',
+        price: '$48',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'omi masu salad',
+        price: '$21',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'temaki whit crab',
+        price: '$32',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'california rolls',
+        price: '$22',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+    ],
+  },
+  {
+    nameMenu: 'main course',
+    plate: [
+      {
+        namePlate: 'braised abalon',
+        price: '$52',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'twice cooked pork',
+        price: '$21',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'kung pao chicken',
+        price: '$32',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'char siu & sushis',
+        price: '$48',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+    ],
+  },
+  {
+    nameMenu: 'desserts',
+    plate: [
+      {
+        namePlate: 'chocolate ball cake',
+        price: '$13',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'lime pie with crust',
+        price: '$14',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'raspberry pear cake',
+        price: '$18',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+      {
+        namePlate: 'café au lait',
+        price: '$6',
+        description: 'Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.',
+      },
+    ],
+  }
+];
+
+export default menu;
